de35b75fe1b54b51c06328137d40afc4e08b415e
Jack Martin <JM273606@gmail.com>
2014-11-13 23:11:11 -0500

    Update
    OOP. bank account application

---
+++ bankaccount.py
@@ -0,0 +1,40 @@
+"""OO Development Bank account model"""
+
+class BankAccount:
+	def __init__(self):
+		self.balance = 0
+
+	def deposit(self, amount):
+		self['balance'] += amount
+		return self.balance
+
+	def withdraw(self, amount):
+		self.balance -= amount
+		return self.balance
+
+class MinimumBalanceAccount(BankAccount):
+	def __init__(self, minimum_balance):
+		BankAccount.__init__(self)
+		self.minimum_balance = minimum_balance
+
+	def withdraw(self, amount):
+		if self.balance - amount < self.minimum_balance:
+			print "Sorry, minimum balance must be maintained"
+		else:
+			BankAccount.withdraw(self, amount)
+		
+
+a = make_account()
+b = make_account()
+
+deposit(a, 100)
+
+deposit(b, 50)
+
+withdraw(b, 10) 
+
+withdraw(a, 10)
+
+
+
+
